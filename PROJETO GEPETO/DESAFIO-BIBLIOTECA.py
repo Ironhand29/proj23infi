e6b50133d067a2fd0d63b444034f6c27dee95232
Victor Gomes <149738778+Ironhand29@users.noreply.github.com>
2023-11-15 15:53:36 -0300

    GEPETO.PY
    15/11/2023
    - Modificado as funções para atender os usuários comuns e 
       administradores
    - Adicionado diferença entre usuários comuns e ADMs
    - Adicionado uma estante de livros alugados para cada usuário ( 
       salvando os livros nela.)
    - Corrigido as funções para atender os novos parâmetros de 
       usuários.
    - Criado nova função para verificar livros em atraso.

--- PROJETO GEPETO/DESAFIO-BIBLIOTECA.py
+++ PROJETO GEPETO/DESAFIO-BIBLIOTECA.py
@@ -6,7 +6,9 @@ def criar_usuario_mestre():
     usuarios = {
         "admin": {
             "cpf": "12345678901",
-            "senha": "007"
+            "senha": "007",
+            "administrador": True,
+            "estante":{}
         }
     }
     salvar_usuarios(usuarios)
@@ -38,7 +40,7 @@ def realizar_login(usuarios):
 
         if usuario in usuarios and usuarios[usuario]["senha"] == senha:
             print("Login bem-sucedido!")
-            return True
+            return usuarios[usuario]
         else:
             tentativas -= 1
             print(f"Tentativas restantes: {tentativas}")
@@ -60,14 +62,10 @@ def visualizar_usuarios(usuarios):
 # FUNÇÃO PARA ALUGAR LIVRO (MENU ALUGUEL, DATA, ETC)
 
 def alugar_livro(usuario, biblioteca, usuarios):
-    if usuario not in usuarios:
-        print("Usuário não encontrado.")
-        return
+    if "estante" not in usuario:
+        usuario["estante"] = {}
 
-    if "livros_alugados" not in usuarios[usuario]:
-        usuarios[usuario]["livros_alugados"] = {}
-
-    if len(usuarios[usuario]["livros_alugados"]) >= 3:
+    if len(usuario["estante"]) >= 3:
         print("Você já atingiu o limite de livros alugados (máximo: 3).")
         return
 
@@ -86,7 +84,7 @@ def alugar_livro(usuario, biblioteca, usuarios):
     dias_aluguel = int(input("Escolha a quantidade de dias para alugar (10, 15 ou 30): "))
     data_devolucao = datetime.now() + timedelta(days=dias_aluguel)
 
-    usuarios[usuario]["livros_alugados"][livro_escolhido] = {
+    usuario["estante"][livro_escolhido] = {
         "data_devolucao": data_devolucao.strftime("%Y-%m-%d"),
         "atrasado": False
     }
@@ -99,13 +97,13 @@ def alugar_livro(usuario, biblioteca, usuarios):
 
 #FUNÇÃO PARA VERIFICAR SE HÁ ATRASOS NA DEVOLUÇÃO DO LIVRO
 
-def verificar_atrasos(usuario, usuarios):
-    if "livros_alugados" not in usuarios[usuario]:
+def verificar_atrasos(estante, usuários):
+    if not estante:
         return False
 
     hoje = datetime.now()
 
-    for livro, detalhes in usuarios[usuario]["livros_alugados"].items():
+    for livro, detalhes in estante.items():
         data_devolucao = datetime.strptime(detalhes["data_devolucao"], "%Y-%m-%d")
 
         if hoje > data_devolucao and not detalhes["atrasado"]:
@@ -113,41 +111,64 @@ def verificar_atrasos(usuario, usuarios):
             detalhes["atrasado"] = True
             print(f"Você está {dias_atraso} dias atrasado na devolução do livro '{livro}'.")
 
+# FUNÇÃO PARA VISUALIZAR LIVROS EM ATRASO
+def visualizar_livros_atrasados(usuarios):
+    atrasos_encontrados = False
+
+    print("\nLivros em Atraso:")
+    for usuario, detalhes_usuario in usuarios.items():
+        if "estante" in detalhes_usuario:
+            for livro, detalhes_livro in detalhes_usuario["estante"].items():
+                if detalhes_livro.get("atrasado", False):
+                    print(f"\nUsuário: {usuario}")
+                    print(f"Título do Livro: {livro}")
+                    print(f"Data de Devolução: {detalhes_livro['data_devolucao']}")
+                    print("----")
+                    atrasos_encontrados = True
+
+    if not atrasos_encontrados:
+        print("Não há livros em atraso no momento.")            
+
 #FUNÇÃO QUE ADMINISTRA O MENU DE ALUGUEL DE LIVROS
 
 def menu_aluguel(usuario, biblioteca, usuarios):
-    verificar_atrasos(usuario, usuarios)
+    verificar_atrasos(usuario["estante"], usuarios)
 
     print("\nOpções do Menu de Aluguel:")
     print("1. Alugar livro")
     print("2. Visualizar livros alugados")
-    print("3. Devolver livro")
-    print("4. Voltar ao Menu Inicial")
+    if usuario.get("administrador", True):
+        print("3. Devolver livro")
+        print("4. Visualizar livros em atraso")
+    print("5. Voltar ao Menu Inicial")
 
-    escolha_menu_aluguel = input("Escolha a opção desejada (1/2/3/4): ")
+    escolha_menu_aluguel = input("Escolha a opção desejada (1/2/3/4/5): ")
 
     if escolha_menu_aluguel == "1":
         alugar_livro(usuario, biblioteca, usuarios)
     elif escolha_menu_aluguel == "2":
         visualizar_livros_alugados(usuario, usuarios)
-    elif escolha_menu_aluguel == "3":
+    elif escolha_menu_aluguel == "3" and usuario.get("administrador", True):
         devolver_livro(usuario, biblioteca, usuarios)
-    elif escolha_menu_aluguel == "4":
+    elif escolha_menu_aluguel == "4" and usuario.get("administrador", True):
+        visualizar_livros_atrasados(usuarios)
+    elif escolha_menu_aluguel == "5":
         salvar_livros(biblioteca)
         salvar_usuarios(usuarios)
         print("Retornando ao Menu Inicial.")
     else:
         print("Opção inválida. Tente novamente.")
 
+
 #FUNÇÃO PARA VERIFICAR LIVROS ALUGADOS
 
 def visualizar_livros_alugados(usuario, usuarios):
-    if "livros_alugados" not in usuarios[usuario] or not usuarios[usuario]["livros_alugados"]:
+    if "estante" not in usuario or not usuario.get("estante", {}):
         print("Você não tem livros alugados no momento.")
         return
 
     print("\nLivros Alugados:")
-    for livro, detalhes in usuarios[usuario]["livros_alugados"].items():
+    for livro, detalhes in usuario["estante"].items():
         status = "Atrasado" if detalhes["atrasado"] else "Em dia"
         print(f"\nTítulo: {livro}")
         print(f"Data de Devolução: {detalhes['data_devolucao']} ({status})")
@@ -155,33 +176,32 @@ def visualizar_livros_alugados(usuario, usuarios):
 #FUNÇÃO PARA DEVOLVER LIVROS ALUGADOS
 
 def devolver_livro(usuario, biblioteca, usuarios):
-    visualizar_livros_alugados(usuario, usuarios)
-
-    if "livros_alugados" not in usuarios[usuario] or not usuarios[usuario]["livros_alugados"]:
+    if "estante" not in usuario or not usuario.get("estante", {}):
         print("Você não tem livros alugados para devolver.")
         return
 
+    visualizar_livros_alugados(usuario, usuarios)
+
     livro_devolver = input("Digite o título do livro que deseja devolver: ")
 
-    if livro_devolver not in usuarios[usuario]["livros_alugados"]:
+    if livro_devolver not in usuario.get("estante", {}):
         print("Você não alugou esse livro.")
         return
 
     hoje = datetime.now()
-    data_devolucao = datetime.strptime(usuarios[usuario]["livros_alugados"][livro_devolver]["data_devolucao"], "%Y-%m-%d")
+    data_devolucao = datetime.strptime(usuario["estante"][livro_devolver]["data_devolucao"], "%Y-%m-%d")
 
     if hoje > data_devolucao:
         dias_atraso = (hoje - data_devolucao).days
         print(f"Você está {dias_atraso} dias atrasado na devolução do livro '{livro_devolver}'.")
-        usuarios[usuario]["livros_alugados"][livro_devolver]["atrasado"] = True
+        usuario["estante"][livro_devolver]["atrasado"] = True
 
     biblioteca[livro_devolver]["quantidade"] += 1
-    del usuarios[usuario]["livros_alugados"][livro_devolver]
+    del usuario["estante"][livro_devolver]
 
     salvar_livros(biblioteca)
     salvar_usuarios(usuarios)
     print(f"Livro '{livro_devolver}' devolvido com sucesso.")
-
 
 #FUNÇÃO PARA CADASTRAR NOVO USUÁRIO
 
@@ -230,7 +250,7 @@ def carregar_livros():
 # FUNÇÃO PARA ADICIONAR LIVROS A BIBLIOTECA, LIVROS COLOCADOS AQUI SÃO SALVOS PELO MENU DA BIBLIOTECA EM UM TERMINAL JSON
 def adicionar_livros(biblioteca):
     livros_para_adicionar = {
-        # ... adicione os outros livros aqui
+        # ... adicione aqui
     }
 
     for livro, detalhes in livros_para_adicionar.items():
@@ -254,14 +274,16 @@ def visualizar_biblioteca(biblioteca):
         print("A biblioteca está vazia.")
 
 # FUNÇÃO QUE CONTROLA O MENU INICIAL DA GEPETO (MENU DE SELEÇÃO SAÍDA USUÁRIO)
-def menu_inicial():
+def menu_inicial(usuario_info,usuarios):
     print("\nOpções do Menu Inicial:")
     print("1. Visualizar informações da biblioteca virtual")
-    print("2. Adicionar novo livro")
-    print("3. Realizar logout")
-    print("4. Cadastrar novo usuário")
-    print("5. Visualizar usuários cadastrados")
-    print("6. Menu de Aluguel de Livros")
+    print("2. Realizar logout")
+    print("3. Menu de Aluguel de Livros")
+    if usuario_info.get("administrador", True):
+        print("4. Adicionar novo livro")
+        print("5. Cadastrar novo usuário")
+        print("6. Visualizar usuários cadastrados")
+      
     return input("Escolha a opção desejada: ")
 
 # FUNÇÃO DE ESCOLHA DAS OPÇÕES DE MENU - CONTROLE DE MENU
@@ -273,35 +295,36 @@ def main():
         usuarios = carregar_usuarios()
 
     # REALIZA LOGIM
-    if realizar_login(usuarios):
-        usuario = input("Digite o nome de usuário: ")
+    usuario_info = realizar_login(usuarios)
+    if usuario_info:
         print("Bem-vindo ao PROJETO GEPETO!")
 
         # INICIALIZA A BIBLIOTECA VIRTUAL
         biblioteca_virtual = carregar_livros()
 
-        while True:
-            escolha_menu = menu_inicial()
+    while True:
+        escolha_menu = menu_inicial(usuario_info, usuarios)
 
-            if escolha_menu == "1":
-                visualizar_biblioteca(biblioteca_virtual)
+        if escolha_menu == "1":    #Visualizar informações da biblioteca virtual
+            visualizar_biblioteca(biblioteca_virtual)
+                    
+        elif escolha_menu == "4" and usuario_info.get("administrador", True):  #Adicionar novo livro
+            adicionar_livros(biblioteca_virtual)
 
-            elif escolha_menu == "2":
-                adicionar_livros(biblioteca_virtual)
+        elif escolha_menu == "2":  #Realizar logout
+            salvar_livros(biblioteca_virtual)
+            print("Logout realizado. Encerrando o programa.")
+            break
 
-            elif escolha_menu == "3":
-                salvar_livros(biblioteca_virtual)
-                print("Logout realizado. Encerrando o programa.")
-                break
+        elif escolha_menu == "5" and usuario_info.get("administrador", True):
+            cadastrar_novo_usuario(usuarios)
+        elif escolha_menu == "6" and usuario_info.get("administrador", True):
+            visualizar_usuarios(usuarios)
+        elif escolha_menu == "3":
+            menu_aluguel(usuario_info, biblioteca_virtual, usuarios)
+        else:
+            print("Opção inválida. Tente novamente.")
 
-            elif escolha_menu == "4":
-                cadastrar_novo_usuario(usuarios)
-            elif escolha_menu == "5":
-                visualizar_usuarios(usuarios)
-            elif escolha_menu == "6":
-                menu_aluguel(usuario, biblioteca_virtual, usuarios)
-            else:
-                print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
     main()
